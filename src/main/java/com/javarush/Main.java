fcd16f726381144d47f6960fa38bf8f8b3e4947c
pel7373 <pel7373@gmail.com>
2024-02-28 12:30:39 +0200

    Initial 2

--- src/main/java/com/javarush/Main.java
+++ src/main/java/com/javarush/Main.java
@@ -1,5 +1,12 @@
-package com.javarush;public class Main {
+package com.javarush;
+
+import redis.clients.jedis.Jedis;
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Jedis jedis = new Jedis("localhost", 6379);
+        String john = jedis.get("username");
+        System.out.println(john);
+
     }
 }
